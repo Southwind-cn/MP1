f9340a037c2bd37b1350904435dfd8aec15e0c06
unknown <github 1587421369@qq.com>
2025-04-06 17:04:10 +0800

    update test

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,8 +3,8 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = 'Wang Weihan'
+export const headline = 'Student at Nanjing Normal University.'
 export const introduction =
   "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
 export const email = 'tong.hu@scls-sh.org'
